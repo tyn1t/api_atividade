e65d54eecffbc575d73f46c1f019cb86b79871d4
Giovanni <anderlansouza71@gmail.com>
2023-01-16 17:38:32 -0400

    alterado nosso ResT API para realiza autenticação no banco de dados. im models foi criado tabela de usuarios com login e senha para autenticação nas APIs. Incluindo em requeriments a extensão flask-httpauth.

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask ,request
 from flask_restful import Resource, Api
-from models import Pessoas, Atividades
+from models import Pessoas, Atividades, Usuarios
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 api = Api(app)
 
+#USUARIOS = {
+#    'Anderlan':'123',
+ #   'galleani':'321'
+ #   }
+
+
+#@auth.verify_password
+#def varificacao(login, senha):
+#    if not (login,senha):
+#        return False
+#    return USUARIOS.get('login') == 'senha':
+
+@auth.verify_password
+def varificacao(login, senha):
+    if not (login,senha):
+        return False
+    return Usuarios.query.filter_by(login=login, senha=senha).first()
+
 class Pessoa(Resource):
+    @auth.login_required
     def get(self, nome):
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         try:
@@ -42,6 +64,7 @@ class Pessoa(Resource):
         return {'status':'sucesso','mensagem':mensagem}
 
 class ListaPessoas(Resource):
+    @auth.login_required
     def get(self):
         pessoas = Pessoas.query.all()
         response = [{'id':i.ids,'nome':i.nome, 'idade':i.idade} for i in pessoas]
--- utils.py
+++ utils.py
@@ -1,10 +1,18 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 # Insere dados na tabela pessoa
 def insere_pessoas():
     pessoa = Pessoas(nome="Galleani", idade=29)
     print(pessoa)
     pessoa.save()
+
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def Consulta_todos_usuario():
+    usuario = Usuarios.query.all()
+    print(usuario)
     
 # Realiza consulta na tabela pessoas
 def consulta_pessoas():
@@ -18,14 +26,21 @@ def altera_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Galleani').first()
     pessoa.nome = 'Felipe'
     pessoa.save()
-
+def exclui_usuario(login):
+    usuario = Usuarios.query.filter_by(login=login).first()
+    usuario.delete()
 # Exclui daddos na tabela pessoa
 def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Felipe').first()
     pessoa.delete()
 
 if __name__=="__main__":
+    insere_usuario('A','123456')
+    insere_usuario('p','1234567')
+    #exclui_usuario('A')
+    Consulta_todos_usuario()
+
     #insere_pessoas()
-    consulta_pessoas()
+    #consulta_pessoas()
     #altera_pessoa()
-    exclui_pessoa()
+    #exclui_pessoa()
